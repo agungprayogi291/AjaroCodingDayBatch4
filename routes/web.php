51b8d072ae1992f14bf9cba5ac607970d50b2eaa
agungprayogi <agungprayogi291@gmail.com>
2021-01-25 23:22:03 +0700

    add layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ExampleController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CategorisController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/starter',[CategorisController::class,'index']);
