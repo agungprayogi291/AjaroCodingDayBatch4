710da183fcea586cce93752b1958531a2f7e8cba
agungprayogi <agungprayogi291@gmail.com>
2021-01-25 23:55:44 +0700

    modifikasi layout ;memecah app layout, add folder categoris

--- app/Http/Controllers/CategorisController.php
+++ app/Http/Controllers/CategorisController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class CategorisController extends Controller
 {
     public function index(){
-        return view('starter');
+        return view('layouts.app');
     }
 }
---
+++ resources/views/layouts/components/footer.blade.php
@@ -0,0 +1,19 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="{{asset('adminLte/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('adminLte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('adminLte/dist/js/adminlte.min.js')}}"></script>
